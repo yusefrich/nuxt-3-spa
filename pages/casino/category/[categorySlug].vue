ef9ff6325088f2c5f9f3593a22afa27ffc5d4506
Davnoronha <davidnd97@gmail.com>
2023-11-08 17:09:18 -0300

    fix: fixing wrong component call

--- pages/casino/category/[categorySlug].vue
+++ pages/casino/category/[categorySlug].vue
@@ -2,12 +2,14 @@
   <sk-category-games v-if="getLayout() === 'SK'" :live="live" />
   <sg-category-games v-else-if="getLayout() === 'SG'" :live="live" />
   <md-category-games v-else-if="getLayout() === 'MD'" :live="live" />
+  <fb-casino-home v-else />
 </template>
 
 <script setup>
 
 import { useLayoutStore } from '@/stores/layout'
 
+import FbCasinoHome from '@/components/fb/templates/FbCasinoHome'
 import SkCategoryGames from '@/components/sk/templates/SkCategoryGames'
 import SgCategoryGames from '~/components/sg/templates/SgCategoryGames'
 import MdCategoryGames from '@/components/md/templates/MdCategoryGames'
--- pages/casino/index.vue
+++ pages/casino/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <sk-casino v-if="layout.getCurrentLayoutStyle === 'SK'" />
+  <sg-home v-if="layout.getCurrentLayoutStyle === 'SG'" />
+  <sk-casino v-else-if="layout.getCurrentLayoutStyle === 'SK'" />
   <md-casino v-else-if="layout.getCurrentLayoutStyle === 'MD'" />
+  <fb-casino-home v-else :top-offset="layout.getCurrentLayoutStyle === 'default'?99:0" />
 </template>
 
 <script setup>
@@ -9,6 +11,7 @@ import { useLayoutStore } from '@/stores/layout'
 
 import SkCasino from '@/components/sk/templates/SkCasino'
 import MdCasino from '@/components/md/templates/MdCasino'
+import FbCasinoHome from '@/components/fb/templates/FbCasinoHome'
 
 const layout = useLayoutStore()
 
